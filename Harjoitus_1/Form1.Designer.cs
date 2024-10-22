8af1d2949b990c96e44598d531af7e4cd8e19bf5
Oskar879 <oskar.molsa@gmail.com>
2024-10-22 09:21:22 +0300

    OtsikkoLB.Text muutos

--- Harjoitus_1/Form1.Designer.cs
+++ Harjoitus_1/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.VaihdaBT = new System.Windows.Forms.Button();
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // VaihdaBT
+            // 
+            this.VaihdaBT.Location = new System.Drawing.Point(338, 259);
+            this.VaihdaBT.Name = "VaihdaBT";
+            this.VaihdaBT.Size = new System.Drawing.Size(75, 23);
+            this.VaihdaBT.TabIndex = 0;
+            this.VaihdaBT.Text = "button1";
+            this.VaihdaBT.UseVisualStyleBackColor = true;
+            this.VaihdaBT.Click += new System.EventHandler(this.VaihdaBT_Click);
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Location = new System.Drawing.Point(360, 155);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(38, 15);
+            this.OtsikkoLB.TabIndex = 1;
+            this.OtsikkoLB.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Controls.Add(this.VaihdaBT);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button VaihdaBT;
+        private Label OtsikkoLB;
     }
 }
